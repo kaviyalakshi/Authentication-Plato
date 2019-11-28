d501855b41b76e0d92a9d732b89cf2a265e698c4
kaviyalakshi <kaviya.m@pickyourtrail.com>
2019-11-28 23:34:12 +0530

    User Auth3

--- src/main/java/com/pyt/jdbctemp/repository/impl/UserRepositoryImplementation.java
+++ src/main/java/com/pyt/jdbctemp/repository/impl/UserRepositoryImplementation.java
@@ -1,10 +1,10 @@
 package com.pyt.jdbctemp.repository.impl;
 
-
-import org.hibernate.SessionFactory;
-import org.hibernate.query.NativeQuery;
-import org.hibernate.query.Query;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.query.NativeQuery;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,12 +14,20 @@ import com.pyt.jdbctemp.repository.UserRepository;
 @Repository
 @Transactional
 public abstract class UserRepositoryImplementation implements UserRepository {
-    @Autowired
-    private SessionFactory sessionFactory;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	@Autowired
+
     @Override
+//    public User getUserByUsername(String username) {
+//        Query<User> query = sessionFactory.getCurrentSession().createQuery("FROM user u where u.username=:username", User.class);
+//        query.setParameter("username", username);
+//        return query.uniqueResult();
+//    }
     public User getUserByUsername(String username) {
-        Query<User> query = sessionFactory.getCurrentSession().createQuery("FROM user u where u.username=:username", User.class);
-        query.setParameter("username", username);
-        return query.uniqueResult();
-    }
+//    	List <String> usernameList = new ArrayList<>(username);
+    	 User a = (User) jdbcTemplate.queryForList("select name from user where username=:username;", String.class);
+		return a;
+		
+	}
 }
